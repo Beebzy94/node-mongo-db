c06ca98f9c338accf022d3a26ff3bf1c2659ae31
nueldona <segunaiyedona@gmail.com>
2025-04-12 00:00:20 +0100

    read products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,3 +12,13 @@ router.post('/add', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+// READ
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
