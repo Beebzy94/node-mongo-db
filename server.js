ec6aba5e422f5cbc7283b1c4050d687272487e0f
nueldona <segunaiyedona@gmail.com>
2025-04-11 23:36:08 +0100

    setup mongo db server

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./db');
+const productRoutes = require('./routes/productRoutes');
+
 const app = express();
 app.use(express.json());
-app.listen(PORT, () => console.log("Server is running"));
+
+connectDB();
+
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
