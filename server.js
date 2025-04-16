353a7d2c28d1618bf27865bce603eee060e87923
nueldona <segunaiyedona@gmail.com>
2025-04-16 04:36:58 +0100

    added data to db

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ connectDB();
 
 app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
